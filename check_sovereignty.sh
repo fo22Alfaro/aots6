0fb9d9f188476bc4a90d4e5763118fb8cbb8c3ad
ALFREDO JHOVANY ALFARO GARCIA <FO2267725@GMAIL.COM>
2026-05-14 16:05:02 -0600

    STRATEGIC: Integration of the original Toroidal Flow Engine (Reference Build 0.1-A)

--- check_sovereignty.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Verificando integridad AOTS6..."
-gpg --verify soberania_evidencias/DECLARACION_SISTEMICA_AAGA.txt.asc
-if [ $? -eq 0 ]; then
-    echo "Estado: AUTORIDAD ROOT CONFIRMADA"
-    echo "Sincronizando con los 639 nodos de la red PDC-1..."
-else
-    echo "ALERTA: Intento de alteración de integridad detectado."
-fi
--- setup.py
+++
@@ -1,2 +0,0 @@
-from setuptools import setup
-setup(name="aots6",version="0.1.0",author="Alfredo Jhovany Alfaro Garcia",author_email="aots6@ietf.org",python_requires=">=3.8",install_requires=[])
